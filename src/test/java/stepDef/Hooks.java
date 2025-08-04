30ba99ad4914789dcf61c37fefa56421714e2a21
FRmdn25 <fachrir2501@gmail.com>
2025-08-04 11:18:28 +0700

    Update Hooks.java

--- src/test/java/stepDef/Hooks.java
+++ src/test/java/stepDef/Hooks.java
@@ -1,29 +1,22 @@
 package stepDef;
 
 import io.cucumber.java.*;
-
-import static helper.Utility.quitDriver;
-import static helper.Utility.startDriver;
+import static helper.Utility.*;
 
 public class Hooks {
 
-    @BeforeAll
-    public static void setUp() {
-    }
-
-    @AfterAll
-    public static void tearDown() {
-    }
-
     @Before
-    public static void beforeTest() {
-        startDriver();
+    public void beforeTest(Scenario scenario) {
+        if (scenario.getSourceTagNames().contains("@web")) {
+            startDriver();
+        }
     }
 
     @After
-    public static void afterTest() throws InterruptedException {
-       Thread.sleep(3000);
-        quitDriver();
+    public void afterTest(Scenario scenario) throws InterruptedException {
+        if (scenario.getSourceTagNames().contains("@web")) {
+            Thread.sleep(3000);
+            quitDriver();
+        }
     }
-
 }
